b1b1b6ceae3ced4b7205e035163b1225b9cb8730
Chaiwat <first582369741@gmail.com>
2024-03-10 11:49:41 +0700

    Initial Project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Leap Year
+
+A leap year is the year that meet the following condition:
+1. Divisible by 4
+2. Not divisible by 100
+3. Divisible by 400
---
+++ chronology/leap_year.py
@@ -0,0 +1,12 @@
+"""Leap Year utility."""
+
+# Standed Library
+
+# 3rd Party Library
+
+# Project Library
+
+
+def is_leap_year(year: int):
+    """Determine if year is a leap year."""
+    return False
